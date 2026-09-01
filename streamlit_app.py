b61239b82c218ac3a18fc4140ff4c1687856bac8
zseidel18 <Zseidel@syr.edu>
2026-09-01 14:49:59 +0000

    finished part a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-lab1_page = st.page('labs/lab1.py', title = 'lab 1')
-lab2_page = st.page('labs/lab2.py', title = 'lab 2')
+lab1_page = st.Page('labs/lab1.py', title = 'lab 1')
+lab2_page = st.Page('labs/lab2.py', title = 'lab 2', default=True)
 
 pg = st.navigation([lab1_page, lab2_page])
 st.set_page_config(page_title= 'lab manager')
