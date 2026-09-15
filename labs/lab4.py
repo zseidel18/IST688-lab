387497fe17d28f4c4b575699a25315db694e83c5
zseidel18 <Zseidel@syr.edu>
2026-09-15 16:05:13 +0000

    comment out testing

--- labs/lab4.py
+++ labs/lab4.py
@@ -106,36 +106,36 @@ st.title('Lab 4: Chatbot using RAG')
 
 
 #### QUERYING A COLLECTION -- ONLY USED FOR TESTING ####
-
-topic = st.sidebar.text_input(
-    'Topic',
-    placeholder='Type your topic (e.g., GenAI)...'
-)
-
-if topic:
-    client = st.session_state.openai_client
-    response = client.embeddings.create(
-        input=topic,
-        model='text-embedding-3-small'
-    )
-
-    # Get the embedding
-    query_embedding = response.data[0].embedding
-
-    # Get the text related to this question (this prompt)
-    results = collection.query(
-        query_embeddings=[query_embedding],
-        n_results=3  # The number of closest documents to return
-    )
-
-    # Display the results
-    st.subheader(f'Results for: {topic}')
-
-    for i in range(len(results['documents'][0])):
-        doc = results['documents'][0][i]
-        doc_id = results['ids'][0][i]
-
-        st.write(f'**{i+1}. {doc_id}**')
-
-else:
-    st.info('Enter a topic in the sidebar to search the collection')
+#
+#topic = st.sidebar.text_input(
+#    'Topic',
+#    placeholder='Type your topic (e.g., GenAI)...'
+#)
+#
+#if topic:
+#    client = st.session_state.openai_client
+#    response = client.embeddings.create(
+#        input=topic,
+#        model='text-embedding-3-small'
+#    )
+#
+#    # Get the embedding
+#    query_embedding = response.data[0].embedding
+#
+#    # Get the text related to this question (this prompt)
+#    results = collection.query(
+#        query_embeddings=[query_embedding],
+#        n_results=3  # The number of closest documents to return
+#    )
+#
+#    # Display the results
+#    st.subheader(f'Results for: {topic}')
+#
+#    for i in range(len(results['documents'][0])):
+#        doc = results['documents'][0][i]
+#        doc_id = results['ids'][0][i]
+#
+#        st.write(f'**{i+1}. {doc_id}**')
+#
+#else:
+#    st.info('Enter a topic in the sidebar to search the collection')
